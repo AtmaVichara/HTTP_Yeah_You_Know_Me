ff52181b8c507f5061f5cc336d6b6c3e422917b3
Joe Jobes <jmrjobes@gmail.com>
2017-12-17 08:26:10 -0700

    Setting up Master Branch

--- lib/server.rb
+++ lib/server.rb
@@ -1,16 +1,37 @@
 require 'socket'
 
-def Socket
+class Server
 
   def initialize(port)
-    @server = TCPServer.new(port)
-    @listener = server.accept
+    @port = port
+    @server = TCPServer.new(@port)
+  end
+
+  attr_reader :server, :listener, :port
+
+  def run_server
+    counter = 0
+    loop do
+      client = server.accept
+      request_lines = client_request(client)
+      formatted_request = format_request(request_lines)
+      puts "Starting Up Server"
+      formatted_request = formatted_request.map do |request|
+        format_paragraph(request)
+      end.join(" ")
+      response = "Hello World (#{counter}) <p>#{formatted_request}</p>"
+      output = format_output(response)
+      header = headers(output)
+      client.puts header
+      client.puts output
+      client.close
+      counter += 1
+    end
   end
 
   private
-  attr_reader :server, :listener
 
-  def client_request
+  def client_request(listener)
     puts "Ready for a request"
     request_lines = []
     while line = listener.gets and !line.chomp.empty?
@@ -19,8 +40,38 @@ def Socket
     request_lines
   end
 
-  def run_server
-
+  def format_request(request)
+    [
+      "Verb: #{request.first.split(' ').first}",
+      "Path: #{request.first.split(' ')[1]}",
+      "Protocol: #{request.first.split(' ')[2]}",
+      "#{request[1].split('')[0..14].join}",
+      "Port: #{request[1].split('')[-4..-1].join}",
+      "Origin: #{request[1].split('')[6..14].join}",
+      "#{request[6]}"
+    ]
   end
+
+  def format_paragraph(paragraphs)
+    "<p> #{paragraphs} </p>"
+  end
+
+  def headers(output)
+    ["http/1.1 200 ok",
+      "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
+      "server: ruby",
+      "content-type: text/html; charset=iso-8859-1",
+      "content-length: #{output.length}\r\n\r\n"].join("\r\n")
+  end
+
+  def format_output(response)
+    "<html><head></head><body>#{response}</body></html>"
+  end
+
+end
+
+if __FILE__ == $0
+  server = Server.new(9292)
+  server.run_server
 
 end
